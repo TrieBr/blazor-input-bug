459a49dace50aedf8d217a723d7311c40536f9b8
Brad Triebwasser <triebr@gmail.com>
2024-02-24 16:12:17 -0800

    first commit

---
+++ BlazorInputBug/BlazorInputBug.Client/Pages/EditForm.cs
@@ -0,0 +1,44 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Rendering;
+namespace BlazorInputBug.Client.Pages;
+
+
+public class ThisCanBeAnyClass
+{
+    public bool IsLoading { get; set; }
+}
+
+public class SomeOtherClass {
+    public bool IsLoading { get; set; }
+}
+
+public class EditForm : Microsoft.AspNetCore.Components.Forms.EditForm
+{
+    [CascadingParameter(Name = "ThisCanBeAnyClass")]
+    public ThisCanBeAnyClass? ThisCanBeAnyClass { get; set; }
+
+    public SomeOtherClass SomeOtherClass { get; set; } = new();
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        Debug.Assert(EditContext != null);
+        builder.OpenRegion(EditContext.GetHashCode());
+
+
+        builder.OpenComponent<CascadingValue<SomeOtherClass>>(1);
+        builder.AddComponentParameter(0, "IsFixed", false);
+        builder.AddComponentParameter(1, "Name", "SomeOtherClass");
+        builder.AddComponentParameter(2, "Value", SomeOtherClass);
+        builder.AddComponentParameter(
+            3,
+            "ChildContent",
+            (RenderFragment)((builder2) => { base.BuildRenderTree(builder2); }
+            ));
+        builder.CloseComponent();
+        builder.CloseRegion();
+    }
+
+
+}
